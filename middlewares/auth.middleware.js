473b5ed8dcaa3c69b913290be864465f4e26bf7d
ANSHIDA SHIRIN <gopikapenakkuzhiyil@gmail.com>
2025-06-29 22:07:47 +0530

    aa

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,19 +1,20 @@
 import { validateToken } from "../libs/token.js"
 
 export async function authMiddleware(req, res, next) {
-    const token = await req.headers.token
+    const token =  req.headers.token
     console.log(token)
-    if(token){
-       const validate =await validateToken(token)
-       if(validate===null){
-       
-         res.send({failed:true,message:"authentication failed"})
-       }
-    else{
-         req.user= validate.userId
-        next()
-    }}
-    else{
-        res.send({failed:true,message:"authentication failed"})
+    if (token) {
+        const validate = await validateToken(token)
+        if (validate === null) {
+
+            res.send({ failed: true, message: "authentication failed" })
+        }
+        else {
+            req.user = validate.userId
+            next()
+        }
+    }
+    else {
+        res.send({ failed: true, message: "authentication failed" })
     }
 }
