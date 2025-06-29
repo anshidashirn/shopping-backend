cf8daf6c3315c19da55974c4659d2f92778b1d26
ANSHIDA SHIRIN <gopikapenakkuzhiyil@gmail.com>
2025-06-29 22:22:24 +0530

    x

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -11,7 +11,7 @@ export async function authMiddleware(req, res, next) {
         }
         else {
             req.user = validate.userId
-            next()
+         next()
         }
     }
     else {
--- models/product.model.js
+++ models/product.model.js
@@ -1,24 +1,23 @@
 import { randomUUID } from "crypto";
 import { Schema ,model} from "mongoose";
-import { type } from "os";
-import { features } from "process";
+
 
 export const productSchema=new Schema({
     _id:{type:String,default:randomUUID}
-    ,name:{type:String,require:true}
+    ,name:{type:String,required:true}
    
-    ,price:{type:Number,require:true}
+    ,price:{type:Number,required:true}
    
-    ,Ram:{type:String,require:true}
-    ,Storage:{type:String,require:true}
-    ,battery:{type:String,require:true}
-    ,displaySize:{type:String,require:true}
-    ,camera:{type:String,require:true}
-    ,os:{type:String,require:true}
-    ,color:{type:String,require:true}
+    ,Ram:{type:String,required:true}
+    ,Storage:{type:String,required:true}
+    ,battery:{type:String,required:true}
+    ,displaySize:{type:String,required:true}
+    ,camera:{type:String,required:true}
+    ,os:{type:String,required:true}
+    ,color:{type:String,required:true}
     ,releaseDate:{type:Date,default:Date.now}
     
-    ,image:{type:String,require:true}
+    ,image:{type:String,required:true}
     
   
     
